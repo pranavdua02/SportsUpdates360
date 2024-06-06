8b9fd5b93e53ab298170f858fec2c2e432254147
pranavdua02 <duapranav11@gmail.com>
2024-06-06 18:29:14 +0530

    sports update create

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nvfj4f069f"
+    "revision": "0.5e8i5ffl398"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
